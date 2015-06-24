b2aada43a38352e15390399e409596b5e8ad90b3
alaaelgndy <alaaelgndy21@yahoo.com>
2015-06-24 14:07:54 +0200

    a

--- admin/add_hotel.php
+++ admin/add_hotel.php
@@ -165,6 +165,7 @@ require 'sidebar.php';
         </div><!-- /.box-body -->
       </form>
 
+      <div class="box-footer">
          <div class="box-footer">
         <button type="submit" name="submit" id="add_all" class="btn btn-primary">add</button>
         <a class="btn btn-success pull-right add_another_form" >add more hotel</a>
