4d805e4da5da3c425bc409d216c03851f3935853
ahmedsammygeek <ahmedsamigeek@gmail.com>
2015-06-24 18:42:52 +0200

    fix

--- admin/delete_hotel.php
+++ admin/delete_hotel.php
@@ -12,6 +12,18 @@ try {
 	$query = $conn->prepare("DELETE FROM hotels WHERE id=?");
 	$query->bindValue(1,$hotel_id,PDO::PARAM_INT);
 	$query->execute();
+
+	$pics = $conn->prepare("SELECT pic FROM hotel_images WHERE hotel_id=?");
+	$pics->bindValue(1,$hotel_id,PDO::PARAM_INT);
+	$pics->execute();
+
+	while ($pic = $pics->fetch(PDO::FETCH_OBJ)) {
+		if(file_exists('../uploaded/hotels_images/'.$pic->pic)) {
+			unlink('../uploaded/hotels_images/'.$pic->pic);
+		}
+	}
+
+
 	/*delete this hotel images*/
 	$query2 = $conn->prepare("DELETE FROM hotel_images WHERE hotel_id=?");
 	$query2->bindValue(1,$hotel_id,PDO::PARAM_INT);
